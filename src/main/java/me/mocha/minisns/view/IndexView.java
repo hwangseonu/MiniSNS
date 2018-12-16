cc32375494b4321d559028c4f9a33c7d2b2bfbd6
hwangseonu <hwangseonu12@naver.com>
2018-12-16 18:20:48 +0900

    init: initialize project

---
+++ src/main/java/me/mocha/minisns/view/IndexView.java
@@ -0,0 +1,18 @@
+package me.mocha.minisns.view;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/"})
+public class IndexView extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(req, res);
+    }
+}
