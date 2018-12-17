4e23c9fc9623adc8b34ffe6ace8a6a7584ee4e3d
hwangseonu <hwangseonu12@naver.com>
2018-12-17 17:01:59 +0900

    feat: Add login

--- src/main/java/me/mocha/minisns/controller/LoginController.java
+++ src/main/java/me/mocha/minisns/controller/LoginController.java
@@ -7,16 +7,17 @@ import me.mocha.minisns.model.service.UserService;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 
     private final UserService userService = new UserService();
+    private final Logger log = Logger.getLogger(getClass().getName());
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -29,13 +30,13 @@ public class LoginController extends HttpServlet {
             if (user.verify(password)) {
                 req.getSession().setAttribute("user", user);
                 res.sendRedirect(req.getContextPath() + "/");
-            } else {
-                RequestDispatcher dispatcher = req.getRequestDispatcher("/login.jsp");
-                req.setAttribute("message", "login failed!");
-                dispatcher.forward(req, res);
+                return;
             }
         } catch (ApplicationException ex) {
-            res.sendError(ex.getStatus());
+            log.warning("login error : " + ex.getMessage());
         }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/login.jsp");
+        req.setAttribute("message", "login failed!");
+        dispatcher.forward(req, res);
     }
 }
