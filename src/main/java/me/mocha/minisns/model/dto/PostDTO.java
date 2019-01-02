54542c885076ab118d04b4a3b2a94cac4ba4141c
hwangseonu <hwangseonu12@naver.com>
2019-01-02 16:53:50 +0900

    feat: Add post model

---
+++ src/main/java/me/mocha/minisns/model/dto/PostDTO.java
@@ -0,0 +1,34 @@
+package me.mocha.minisns.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+public class PostDTO {
+
+    @Getter
+    private long id;
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private String content;
+
+    @Getter
+    private int views;
+
+    @Getter
+    private String username;
+
+    public PostDTO addViews(int i) {
+        this.views += i;
+        return this;
+    }
+
+}
